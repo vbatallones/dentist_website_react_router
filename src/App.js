fab1602f2ae0f99e32e49049b08239c2d9dc3399
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-28 10:59:55 -0700

    [contact, procedures] Add contact and procedures components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './Home';
+import Procedures from './Procedures';
+import Contact from './Contact'
 function App() {
 	return <div className="App">
     <Home />
+    <Procedures />
+    <Contact />
   </div>;
 }
 
---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Contact Us</h1>
+                <p>Levin Batallones, MD</p>
+                <p>Email: batallones@dentist.com</p>
+                <p>Phone: 111-111-1111</p>
+            </div>
+        )
+    }
+}
+
+
+export default Contact;
