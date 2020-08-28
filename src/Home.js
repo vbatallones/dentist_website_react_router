391730e149f9123d7b38bc4b366b12a9a34c7b85
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-28 10:52:42 -0700

    [home] add home component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Levin Dentistry</h1>
+				<p>Where the dentist go for their dental needs.</p>
+			</div>
+		);
+	}
+}
+
+export default Home;
