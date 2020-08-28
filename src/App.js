1b6b98d8e440e819645ac5723d68fa21aa79f366
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-28 11:30:21 -0700

    [procedures] Add procedures as a props into component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-// this is the router dom/ boiler-plate
+//  this is the router dom / boiler-plate
 import {
   BrowserRouter as Router,
   Route,
@@ -12,18 +12,35 @@ import Home from './Home';
 import Procedures from './Procedures';
 import Contact from './Contact'
 function App() {
+  const procedures = [
+    'Deep Cleaning',
+    'Oral Surgery',
+    'Root Canal',
+    'Crown',
+    'Implants',
+    'Fillings'
+  ];
+  
 	return (
+
+    // wrap everything in the ROUTER
     <Router>
       <div className="App">
+
       <nav>
+      {/* keyword for the link to work is the " to="" " */}
         <Link to="/">Home</Link>{' '}
         <Link to="/procedures">See our Procedures</Link>{' '}
         <Link to="/contact">Contact Us!</Link>{' '}
       </nav>
-      {/* the exact will be just for the home and it is not going to render other page */}
+
+      {/* *ROUTES* the exact will be just for the home and it is not going to render other page */}
+      {/* give every main component a ROUTE and path. */}
         <Route exact path="/" component={Home} />
-        <Route path="/procedures" component={Procedures} />
+        {/* the render method will let you use the array we did to pass all the items into the Procedures page */}
+        <Route path="/procedures" render={() => <Procedures procedures={procedures} />} />
         <Route path="/contact" component={Contact} />
+
       </div>
     </Router>
   )
